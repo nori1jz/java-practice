4d35fcee349b5901ab3c0a30674a1e8ab3f56efb
小笠原紀大 <norihiro@kidainoMacBook-Air.local>
2021-12-24 02:20:48 +0900

    [Finish]~Chapter6

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "小笠原";
+		System.out.println(str1);
+		
+		str1 = "元気があればなんでもできる！";
+		System.out.println(str1);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
